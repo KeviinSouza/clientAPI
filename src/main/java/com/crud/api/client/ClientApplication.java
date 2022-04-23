a5891b0ad68c2c6c237e14a5d1fc427c3e000958
Kevin Souza <kevinsou113@gmail.com>
2022-04-23 19:34:01 -0300

    Implementação Swagger
    Arquivo de config, e ajustes em classes existentes para utilização do swagger ui como docda API

--- src/main/java/com/crud/api/client/ClientApplication.java
+++ src/main/java/com/crud/api/client/ClientApplication.java
@@ -2,7 +2,12 @@ package com.crud.api.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import io.swagger.annotations.Api;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
 public class ClientApplication {
 
